24f22ecbd36f03b0d4705f863a9807e8edd608ed
Yaroslav <kprogrammer0pr@gmail.com>
2023-08-09 18:44:33 +0300

    Vercel

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use("/api", router);
 // errors
 app.use(errorHandler);
 
-const PORT = process.env.SERVER_PORT || 3030;
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   try {

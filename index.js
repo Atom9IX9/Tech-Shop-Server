51ff2e5de4b66aa76ca314f0840fc0c1f51a20ca
Atom9IX9 <121716655+Atom9IX9@users.noreply.github.com>
2024-02-04 02:30:40 +0200

    Static path index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, "static")));
+app.use("/static", express.static(path.resolve(__dirname, "static")));
 app.use(fileUpload({}));
 app.use("/api", router);
 
